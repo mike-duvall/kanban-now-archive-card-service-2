f685e83b6bb5e350798d8d9765ffee32fc8492b2
Mike DuVall <mike.duvall@blackbaud.com>
2015-12-04 17:00:38 -0600

    Added initial rest integration test using SSL

---
+++ src/test/groovy/com/mike/SampleSpec.groovy
@@ -0,0 +1,92 @@
+package com.mike
+
+import groovyx.net.http.RESTClient
+import org.apache.http.client.HttpResponseException
+import org.apache.http.conn.scheme.Scheme
+import org.apache.http.conn.ssl.SSLSocketFactory
+import spock.lang.Ignore
+import spock.lang.Specification
+import java.security.KeyStore
+
+
+@Ignore
+class SampleSpec extends Specification {
+
+    String host = "localhost"
+    String port = "9000"
+
+    String baseUrl = "https://${host}:${port}/"
+    RESTClient restClient = new RESTClient(baseUrl)
+
+
+
+    def "Sample"() {
+
+        when:
+        String accessKeyId  = System.getProperty("accessKeyId")
+        String secretKey = System.getProperty("secretKey")
+
+        String userAndPassword = accessKeyId + ":" + secretKey
+        String userAndPasswordEncoded = userAndPassword.bytes.encodeBase64().toString()
+        String headerValue = "Basic " + userAndPasswordEncoded
+
+        def keyStore = KeyStore.getInstance( KeyStore.defaultType )
+
+        String keyStoreFilename = System.getProperty("server_ssl_key_store")
+        String keyStorePassword = System.getProperty("server_ssl_key_store_password")
+
+        String keyStorePath = "/" + keyStoreFilename
+
+        getClass().getResource( keyStorePath ).withInputStream {
+            keyStore.load( it, keyStorePassword.toCharArray() )
+        }
+
+
+//        SSLSocketFactory sf = new SSLSocketFactory(
+//                SSLContext.getInstance("TLS"),
+//                SSLSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER);
+
+        SSLSocketFactory sf = new SSLSocketFactory(keyStore);
+        sf.setHostnameVerifier(SSLSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER)
+        Scheme sch = new Scheme("https", 443, sf);
+
+        restClient.client.connectionManager.schemeRegistry.register( sch )
+
+        def response = callRest {
+            restClient.get(
+                    path : '/greeting',
+                    requestContentType:  'application/json',
+                    headers: ["Authorization" : headerValue ]
+
+            )
+        }
+
+
+        then:
+        assert response.responseData.content == "Hello, Mike DuVall!"
+
+    }
+
+    def "test" () {
+        given:
+        int x = 3;
+
+        when:
+        x = 4
+
+        then:
+        true
+    }
+
+
+
+
+    static def callRest(def closure) {
+        try {
+            return closure()
+        } catch (HttpResponseException ex) {
+            return ex.response
+        }
+    }
+
+}
