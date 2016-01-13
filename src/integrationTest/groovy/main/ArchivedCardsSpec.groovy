fb5fab7a68cdb28a40614b8bb26b3605bc08ed99
Mike DuVall <mike.a.duvall@gmail.com>
2016-01-13 10:48:19 -0600

    Updated service to store state internally

---
+++ src/integrationTest/groovy/main/ArchivedCardsSpec.groovy
@@ -0,0 +1,103 @@
+package main
+
+import groovyx.net.http.RESTClient
+import org.apache.http.client.HttpResponseException
+import spock.lang.Ignore
+import spock.lang.Specification
+
+
+class ArchivedCardsSpec extends Specification {
+
+    static final int HTTP_OK = 200
+
+    String host = readTestProperty("serviceHost")
+    String port = readTestProperty("servicePort")
+
+
+    String accessKeyId = readTestProperty("accessKeyId")
+    String secretKey = readTestProperty("secretKey")
+
+
+    String baseUrl = "https://${host}:${port}/"
+    RESTClient restClient = new RESTClient(baseUrl)
+
+    String userAndPassword = accessKeyId + ":" + secretKey
+    String userAndPasswordEncoded = userAndPassword.bytes.encodeBase64().toString()
+    String headerValue = "Basic " + userAndPasswordEncoded
+
+    private static String readTestProperty(String key) {
+        String result = System.getProperty(key)
+        if(result == null) {
+            result = System.getenv(key);
+        }
+
+        return result
+    }
+
+    def "get cards"() {
+        when:
+        def response = callRest {
+            restClient.get(
+                    path : '/archivedCards',
+                    requestContentType:  'application/json',
+                    headers: ["Authorization" : headerValue ]
+
+            )
+        }
+
+        then:
+        assert response.responseData.size == 2
+        assert response.responseData[0].text == "Take Claritin"
+        assert response.responseData[1].text == "Save the whales"
+    }
+
+
+    def "add cards then retrieve them"() {
+
+        when:
+        def newCard1 = [
+                text: 'A new card',
+                date: '1/1/1967'
+        ]
+
+        def postResponse = callRest {
+            restClient.post(
+                    path : '/archivedCards',
+                    body: newCard1,
+                    requestContentType:  'application/json',
+                    headers: ["Authorization" : headerValue ]
+
+            )
+        }
+
+        then:
+        assert postResponse.status == HTTP_OK
+
+        and:
+        def response = callRest {
+            restClient.get(
+                    path : '/archivedCards',
+                    requestContentType:  'application/json',
+                    headers: ["Authorization" : headerValue ]
+
+            )
+        }
+
+        then:
+        assert response.responseData.size == 1
+        assert response.responseData[0].text == newCard1.text
+
+    }
+
+
+
+
+    static def callRest(def closure) {
+        try {
+            return closure()
+        } catch (HttpResponseException ex) {
+            return ex.response
+        }
+    }
+
+}
--- src/main/java/com/stormpath/tutorial/controller/ArchivedCardController.java
+++ src/main/java/com/stormpath/tutorial/controller/ArchivedCardController.java
@@ -5,7 +5,9 @@ import com.stormpath.sdk.servlet.account.AccountResolver;
 import com.stormpath.tutorial.exception.ForbiddenException;
 import org.joda.time.DateTimeZone;
 import org.joda.time.LocalDate;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
@@ -16,26 +18,29 @@ import java.util.List;
 public class ArchivedCardController {
 
 
-    @RequestMapping("/archivedCards")
-    public List<ArchivedCard> archivedCard(HttpServletRequest req) {
+    List<ArchivedCard> archivedCardList = new ArrayList<ArchivedCard>();
+
+
+    @RequestMapping(value = "/archivedCards",  method = RequestMethod.GET)
+    public List<ArchivedCard> getArrchivedCards(HttpServletRequest req) {
 
         Account account = AccountResolver.INSTANCE.getAccount(req);
 
         if (account == null) { throw new ForbiddenException(); }
 
-        List<ArchivedCard> archivedCardList = new ArrayList<ArchivedCard>();
-        ArchivedCard card1 = new ArchivedCard();
-
-        String todayDateAsString = getTodayDateAsString();
-
-        card1.setText("Take Claritin");
-        card1.setDate(todayDateAsString);
-        archivedCardList.add(card1);
-
-        ArchivedCard card2 = new ArchivedCard();
-        card2.setText("Save the whales");
-        card2.setDate(todayDateAsString);
-        archivedCardList.add(card2);
+//        List<ArchivedCard> archivedCardList = new ArrayList<ArchivedCard>();
+//        ArchivedCard card1 = new ArchivedCard();
+//
+//        String todayDateAsString = getTodayDateAsString();
+//
+//        card1.setText("Take Claritin");
+//        card1.setDate(todayDateAsString);
+//        archivedCardList.add(card1);
+//
+//        ArchivedCard card2 = new ArchivedCard();
+//        card2.setText("Save the whales");
+//        card2.setDate(todayDateAsString);
+//        archivedCardList.add(card2);
 
         return archivedCardList;
     }
@@ -50,6 +55,19 @@ public class ArchivedCardController {
         LocalDate localDate = new LocalDate(chicagoTimeZone);
         String dateAsString = localDate.toString("M/d/yyy");
         return dateAsString;
+    }
+
+
+    @RequestMapping(value = "/archivedCards",  method = RequestMethod.POST)
+    public ArchivedCard addArchivedCard(HttpServletRequest req, @RequestBody ArchivedCard archivedCard) {
+
+        Account account = AccountResolver.INSTANCE.getAccount(req);
+
+        if (account == null) { throw new ForbiddenException(); }
+
+        archivedCardList.add(archivedCard);
+
+        return archivedCard;
     }
 
 
